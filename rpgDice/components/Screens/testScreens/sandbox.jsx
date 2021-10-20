1586a1cef3eae287c960b53515115cafd7696bb9
rumbo <rumbo.9370@gmail.com>
2021-10-20 12:24:31 +0200

    Update sandbox.jsx

--- rpgDice/components/Screens/testScreens/sandbox.jsx
+++ rpgDice/components/Screens/testScreens/sandbox.jsx
@@ -5,27 +5,59 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import Styles from "../../../assets/styles/styles";
+import { NavigationContainer, ThemeProvider } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import Style from "../../../assets/styles/styles";
+
+import folders from "./folders";
+import presets from "./preSets";
 
 //storage keys
 //presets
 //folders
 //dice
-
+const Stack = createNativeStackNavigator();
 export default function Sandbox() {
+  const [preSetDefoult, setPreSetDefoult] = useState(false);
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={{ alignSelf: "center" }}>
-        <Text style={[Style.textColor, { fontSize: 40 }]}>
-          welcome to sandbox
-        </Text>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={({ navigation }) => ({
+          headerStyle: {
+            backgroundColor: "#47494E",
+          },
+          headerTintColor: "white",
+          headerTitleAlign: "center",
+          animation: "fade",
+        })}>
+        {preSetDefoult ? (
+          <Stack.Screen name='preset' component={presets} />
+        ) : (
+          <>
+            <Stack.Screen
+              name='Home'
+              component={folders}
+              options={({ navigation }) => ({
+                headerRight: () => (
+                  <View>
+                    <TouchableOpacity
+                      onPress={() => navigation.navigate("preset")}>
+                      <Text style={Style.buttonStyle}>pre sets</Text>
+                    </TouchableOpacity>
+                  </View>
+                ),
+              })}
+            />
+            <Stack.Screen name='preset' component={presets} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#2E2E2E",
@@ -40,4 +72,4 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: "white",
   },
-});
+}); */
